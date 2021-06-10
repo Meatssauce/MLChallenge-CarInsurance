bcbbec95ee44cb27a52793bc4f29e055720bf294
Meatssauce <70827089+Meatssauce@users.noreply.github.com>
2021-06-11 00:18:34 +1000

    initial commit

--- eval_classifier.py
+++ eval_classifier.py
@@ -10,11 +10,6 @@ def main():
     img_height, img_width = 128, 128
     batch_size = 32
 
-    # df = load_data('dataset/train.csv', 'dataset/trainImages/', (128, 128), grey_scale=True)
-    # X, y = df['Images'].to_numpy(), df['Condition'].to_numpy()
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, shuffle=True, random_state=seed)
-    # train_data = tf.data.Dataset.from_tensor_slices((X_train, y_train))
-    # test_data = tf.data.Dataset.from_tensor_slices((X_test, y_test))
     train_ds = tf.keras.preprocessing.image_dataset_from_directory(
         directory='dataset/trainImages/',
         validation_split=0.2,
@@ -36,14 +31,9 @@ def main():
 
     train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
     test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
-    model = ak.ImageClassifier(overwrite=True, max_trials=5)
-    model.fit(train_ds, validation_split=0.2)
+    model = tf.keras.models.load_model('model_autokeras')
     model.evaluate(test_ds)
-
-    try:
-        model.export_model().save("classifier_model_autokeras", save_format="tf")
-    except Exception:
-        model.export_model().save("classifier_model_autokeras.h5")
+    # loss: 0.2085 - accuracy: 0.9355
 
     return
 
